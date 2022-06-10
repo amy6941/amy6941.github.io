5b3500a1bb29ec85d441ae0e293d4672afc0ff06
Use your Github username <Enter your email address>
2022-06-09 18:48:21 -0600

    js update

--- scripts/date.js
+++ scripts/date.js
@@ -1,4 +1,4 @@
-let daynames = ['Sunday', 'Monday', 'Tuesday, 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let daynames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 let d = new Date();
